eeafd9f18980aba35c4eada0193148ec57246718
jenny <jenny@winlab.rutgers.edu>
2019-12-17 17:15:53 -0500

    put construction of toolbars in functions for clarity

--- Player.py
+++ Player.py
@@ -1,6 +1,8 @@
 import sys
 import time
 from Dataset import *
+from Labelset import *
+from labelDialog import *
 
 from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
@@ -10,14 +12,15 @@ from bboxCanvas import *
 
 
 class Player(QMainWindow):
-    def __init__(self, data, *args, **kwargs):
+    def __init__(self, data, labels, *args, **kwargs):
         super(Player, self).__init__(*args, **kwargs)
 
         self.data=Dataset(data)
         self.file_list=[]
         for f in self.data.get_files():
             self.file_list.append(f.split("/")[-1])
-        #self.file_list=self.data.get_files()
+
+        self.labelset=LabelSet(self.file_list, labels[0], labels[1])
         
         self.setWindowTitle("test")
         self.status={"playing":False}
@@ -32,11 +35,46 @@ class Player(QMainWindow):
         dock_widget.setObjectName("Data file list")
         dock_widget.setAllowedAreas(Qt.LeftDockWidgetArea|Qt.RightDockWidgetArea)
         dock_widget.setWidget(self.list_widget)
+        self.addDockWidget(Qt.LeftDockWidgetArea, dock_widget)
 
         self.video_timer=QTimer()
         self.video_timer.timeout.connect(self.next_frame)
         self.video_timer.setInterval(30)
 
+        self.createLabelBar()
+        self.createVideoBar()
+
+        mainWidget=QWidget()
+        layout=QVBoxLayout()
+        layout.addWidget(self.labelBar)
+        layout.addWidget(self.image_frame)
+        layout.addWidget(self.videoBar)
+        mainWidget.setLayout(layout)
+        self.setCentralWidget(mainWidget)
+
+        self.render_frame()
+
+    def createLabelBar(self):
+        frame_labels, bbox_labels=self.labelset.getLabelNames()
+        self.labelBar=QToolBar("labeling")
+        self.labelBar.addWidget(QLabel("Frame Labels: "))
+
+        self.frame_label_selection=QComboBox()
+        self.frame_label_selection.addItems(frame_labels)
+        self.labelBar.addWidget(self.frame_label_selection)
+
+        markFrameAction=QAction("Mark Frame", self)
+        markFrameAction.triggered.connect(self.mark_frame)
+        self.labelBar.addAction(markFrameAction)
+
+        self.labelBar.addWidget(QLabel("Box Labels: "))
+
+        self.box_label_selection=QComboBox()
+        self.box_label_selection.addItems(bbox_labels)
+        self.labelBar.addWidget(self.box_label_selection)
+
+
+    def createVideoBar(self):
         self.videoBar=QToolBar("video playback")
 
         pfileaction=QAction(QIcon("icons/start.png"), "prevfile", self)
@@ -59,23 +97,18 @@ class Player(QMainWindow):
         nfileaction.triggered.connect(self.next_file)
         self.videoBar.addAction(nfileaction)
         
-        self.addDockWidget(Qt.LeftDockWidgetArea, dock_widget)
-
-        self.setCentralWidget(self.image_frame)
-
-        self.addToolBar(Qt.BottomToolBarArea, self.videoBar)
-
-        self.render_frame()
-
+  
     def loadfile(self, i):
         self.data.goto_file(self.list_widget.row(i))
         self.render_frame()
 
     def playpause(self):
         if self.status["playing"]==False:
+            self.image_frame.setEnable(False)
             self.video_timer.start()
             self.status["playing"]=True
         else:
+            self.image_frame.setEnable(True)
             self.video_timer.stop()
             self.status["playing"]=False
 
@@ -113,10 +146,15 @@ class Player(QMainWindow):
         qimg=QImage(self.data.get_current_frame(), 424, 240, 424*3, QImage.Format_RGB888).rgbSwapped()
         self.image_frame.setPixmap(QPixmap.fromImage(qimg))
 
+    def mark_frame(self):
+        pass
+
+    def mark_box(self, x, y, w, h):
+        pass
 
 print(sys.argv)
 app=QApplication(sys.argv)
 
-window=Player(sys.argv[1:])
+window=Player(sys.argv[1:], [["test", "sample"], ["testb", "sampleb"]])
 window.show()
 app.exec_()
