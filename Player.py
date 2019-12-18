77537036133845370de4798928d5710cd7121133
jenny <jenny@winlab.rutgers.edu>
2019-12-18 17:40:06 -0500

    adding stuff for labeling

--- Player.py
+++ Player.py
@@ -24,7 +24,7 @@ class Player(QMainWindow):
         
         self.setWindowTitle("test")
         self.status={"playing":False}
-        self.image_frame=bboxCanvas(424, 240)
+        self.image_frame=bboxCanvas(848, 480)
         self.list_widget=QListWidget()
         self.list_widget.insertItems(0, self.file_list)
         self.list_widget.setMinimumWidth(200)
@@ -52,6 +52,8 @@ class Player(QMainWindow):
         mainWidget.setLayout(layout)
         self.setCentralWidget(mainWidget)
 
+        self.label_range={"start":[0, 0], "end":[0, 0]}
+
         self.render_frame()
 
     def createLabelBar(self):
@@ -63,10 +65,21 @@ class Player(QMainWindow):
         self.frame_label_selection.addItems(frame_labels)
         self.labelBar.addWidget(self.frame_label_selection)
 
-        markFrameAction=QAction("Mark Frame", self)
+        self.lBracketAction=QAction(QIcon("icons/lbracket.png"), "begin label range", self)
+        lBracketAction.setCheckable(True)
+        lBracketAction.triggered.connect(self.l_bracket)
+        self.labelBar.addAction(lBracketAction)
+
+        self.rBracketAction=QAction(QIcon("icons/rbracket.png"), "end label range", self)
+        rBracketAction.setCheckable(True)
+        rBracketAction.triggered.connect(self.r_bracket)
+        self.labelBar.addAction(rBracketAction)
+
+        markFrameAction=QAction(QIcon("icons/flag.png"), "Mark Frame", self)
         markFrameAction.triggered.connect(self.mark_frame)
         self.labelBar.addAction(markFrameAction)
 
+        self.labelBar.addSeparator()
         self.labelBar.addWidget(QLabel("Box Labels: "))
 
         self.box_label_selection=QComboBox()
@@ -143,11 +156,58 @@ class Player(QMainWindow):
                 self.list_widget.setCurrentRow(indices[0])
 
     def render_frame(self):
+        self.image_frame.clear()
         qimg=QImage(self.data.get_current_frame(), 424, 240, 424*3, QImage.Format_RGB888).rgbSwapped()
-        self.image_frame.setPixmap(QPixmap.fromImage(qimg))
+        self.image_frame.setPixmap(QPixmap.fromImage(qimg).scaled(848, 480))
+
+    def l_bracket(self):
+        self.label_range["start"]=self.data.get_indices()
+
+    def r_bracket(self):
+        self.label_range["end"]=self.data.get_indices()
 
     def mark_frame(self):
-        pass
+        current_label=self.frame_label_selection.currentText()
+        if self.lBracketAction.isChecked() and self.rBracketAction.isChecked():
+            filenames=self.data.get_files()
+            if self.label_range["start"][0]==self.label_range["end"][0]:
+                nframes=self.label_range["end"][1]-self.label_range["start"][1]+1
+                message='Assigning label "'+current_label+'" to '+str(nframes)+" frames in file "+fileneames[self.label_range["start"][0]]+'\nOK?'
+                reply=QMessageBox.question(self, "Confirm", message, QMessageBox.Ok|QMessageBox.Cancel)
+                if reply==QMessageBox.Ok:
+                    for i in range(self.label_range["start"][1], self.label_range["end"][1]+1):
+                        self.labelSet.markFrameLabel(self.label_range["start"][0], i, current_label)
+
+            elif self.label_range["start"][0]<self.label_range["end"][0]:
+                message='Assigning label "'+current_label+'" to range from frame# '+str(self.label_range["start"][1])+
+                "in file "+fileneames[self.label_range["start"][0]]+" to frame# "+str(self.label_range["end"][1])+
+                "in file "+fileneames[self.label_range["end"][0]]+".\nOK?"
+                reply=QMessageBox.question(self, "Confirm", message, QMessageBox.Ok|QMessageBox.Cancel)
+                if reply==QMessageBox.Ok:
+                    for i in range(self.label_range["start"][0]+1, self.label_range["end"][0]):
+                        for j in range(0, self.data.n_frames(i)):
+                            self.labelSet.markFrameLabel(i, j, current_label)
+                    for j in range(self.label_range["start"][1], self.data.n_frames(self.label_range["start"][0])):
+                        self.labelSet.markFrameLabel(self.label_range["start"][0], j, current_label)
+                    for j in range(0, self.label_range["end"][1]+1):
+                        self.labelSet.markFrameLabel(self.label_range["end"][0], j, current_label)
+
+            else:
+                box=QMessageBox(self)
+                box.setText("Range not in order")
+                box.exec_()
+
+        elif self.lBracketAction.isChecked():
+            box=QMessageBox(self)
+            box.setText("Label range start is set, but end is not set!")
+            box.exec_()
+        elif self.rBracketAction.isChecked():
+            box=QMessageBox(self)
+            box.setText("Label range end is set, but start is not set!")
+            box.exec_()
+        else:
+            indices=self.data.get_indices()
+            self.labelSet.markFrameLabel(indices[0], indices[1], current_label)
 
     def mark_box(self, x, y, w, h):
         pass
